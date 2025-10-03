44bb9e775d93c954c56e0ad0a993b7e0ac5c5606
DPBascur <danielpa423@gmail.com>
2025-10-03 17:56:07 -0300

    Feat: Vercel Analytics y Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { WorkoutProvider } from "@/contexts/WorkoutContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -98,6 +100,8 @@ export default function RootLayout({
             </div>
           </WorkoutProvider>
         </AuthProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
